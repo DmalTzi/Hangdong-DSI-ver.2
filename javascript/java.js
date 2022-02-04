df03eed7138bb7ae50a04ee8c67fb967126c9192
DmalTzi <Audyomg@gmail.com>
2022-02-04 21:40:06 +0700

    big update

--- javascript/java.js
+++ javascript/java.js
@@ -1,3 +1,19 @@
-function togglePopup(){
-    document.getElementById("popup-1").classList.toggle("active");
-}
+// function togglePopup(){
+//     document.getElementById("popup-1").classList.toggle("active");
+// }
+
+document.addEventListener("click", e => {
+    const isDropdownButton = e.target.matches('[data-dropdown-button]')
+    if (!isDropdownButton && e.target.closest('[data-dropdown]') != null) return
+
+    let currentDropdown
+    if (isDropdownButton) {
+        currentDropdown = e.target.closest('[data-dropdown]')
+        currentDropdown.classList.toggle('active')
+    }
+
+    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+        if (dropdown === currentDropdown) return
+        dropdown.classList.remove('active')
+    })
+})
