fa64752bc6da865e5588c0bd9b477c30bb7f7446
DmalTzi <Audyomg@gmail.com>
2022-02-07 15:46:49 +0700

    .

--- javascript/java.js
+++ javascript/java.js
@@ -2,18 +2,34 @@
 //     document.getElementById("popup-1").classList.toggle("active");
 // }
 
-document.addEventListener("click", e => {
-    const isDropdownButton = e.target.matches('[data-dropdown-button]')
-    if (!isDropdownButton && e.target.closest('[data-dropdown]') != null) return
+// document.addEventListener("click", e => {
+//     const isDropdownButton = e.target.matches('[data-dropdown-button]')
+//     if (!isDropdownButton && e.target.closest('[data-dropdown]') != null) return
 
-    let currentDropdown
-    if (isDropdownButton) {
-        currentDropdown = e.target.closest('[data-dropdown]')
-        currentDropdown.classList.toggle('active')
-    }
+//     let currentDropdown
+//     if (isDropdownButton) {
+//         currentDropdown = e.target.closest('[data-dropdown]')
+//         currentDropdown.classList.toggle('active')
+//     }
 
-    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
-        if (dropdown === currentDropdown) return
-        dropdown.classList.remove('active')
-    })
-})
+//     document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+//         if (dropdown === currentDropdown) return
+//         dropdown.classList.remove('active')
+//     })
+// })
+
+const countElement = document.getElementById('count');
+
+update();
+
+function update(){
+    fetch('https://api.countapi.xyz/update/dmaltzicode/test?amount=1')
+        .then(res => res.json())
+        .then(res => {
+            countElement.innerHTML = res.value;
+        })
+}
+
+function websiteVisits(response){
+    document.querySelector("#visits").textContent = response.value;
+}
